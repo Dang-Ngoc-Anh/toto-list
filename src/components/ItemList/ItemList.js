b4f060ca9d00216d7affcf9d1c6299f3d9c8aa5c
Dang Ngoc Anh <anhcu2101@gmail.com>
2023-11-22 17:17:40 +0700

    update scroll use cumtem hook

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,58 +2,29 @@ import React, { forwardRef, useEffect, useImperativeHandle, useRef, useState } f
 import Item from "../Item/Item";
 import "./item-list.css";
 import { useSelector } from "react-redux";
-import reducerData from "../../store/reducers/reducerData";
+import {useSroll} from '../hook/useScroll'
+import { logDOM } from "@testing-library/react";
 let isGreeting = false;
 const ItemList = (props ,ref) => {
   const listItemRef = useRef();
   const {requestUpdate} = props
   const {reducerData:dataStore} = useSelector(state => state);
-  const [data , setData] = useState(dataStore)
-  const [dataScroll , setDataScroll] = useState(data)
-  const [page , setPage] = useState(1);
-  const [isLoading , setIsLoading] = useState(false);
-  
-  const fetchData = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      const newData = dataStore.slice(page * 5, (page + 1) * 5);
-      setDataScroll(pre => [...pre , ...newData]);
-      setIsLoading(false);
-      setPage((prevPage) => prevPage + 1);
-      // Đẩy thanh scroll lên
-      window.scrollTo({
-        top: listItemRef.current.offsetTop - 100, // Điều chỉnh giảm độ cao để thanh scroll không che phủ hoàn toàn nội dung mới
-        behavior: "smooth",
-      });
-    }, 1000); 
-  };
+  let {dataScroll ,isGreeting} = useSroll(dataStore , listItemRef)
 
-  const infinityScroll = ()=>{
-    const {scrollHeight , clientHeight , scrollTop } = listItemRef.current
-    if((scrollHeight <= (clientHeight + scrollTop + 10)) && !isGreeting){
-        fetchData();
-    }
-  }
-  useEffect(() =>{
-    listItemRef.current.addEventListener('scroll' , infinityScroll)
-  },[isLoading])
+  // useImperativeHandle(ref, () => ({
+  //   displayDataFilter(data){
+  //     dataScroll = data;
+  //   }
+  // }));
 
-  useEffect(() => 
-    setData(dataStore)
-    ,[dataStore]
-  )
+  // console.log(dataScroll);
 
-  useImperativeHandle(ref, () => ({
-    displayDataFilter(data){
-      setData(data);
-    }
-  }));
   return (
     <div className="list__item" ref={listItemRef}>
-      { data.map((item) => (
+      {dataScroll.map((item) => (
           <Item props={item} key={item.id} requestUpdate={requestUpdate}/>
       ))}
-      {isLoading && <div style={{ textAlign: 'center', padding: '20px' }}>Loading...</div>}
+      {isGreeting && <div style={{ textAlign: 'center', padding: '20px' }}>Loading...</div>}
     </div>
   );
 };
---
+++ src/components/hook/useScroll.js
@@ -0,0 +1,36 @@
+import { createRef, useEffect, useState } from "react"
+
+export const useSroll = (data , ref)=>{
+  const [dataScroll , setDataScroll] = useState(data);
+  const [page , setPage] = useState(1);
+  const [isLoading , setIsLoading] = useState(false);
+  let isGreeting = false;
+  const fetchData = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      const newData = data.slice(page * 5, (page + 1) * 5);
+      setDataScroll(pre => [...pre , ...newData]);
+      setIsLoading(false);
+      setPage((prevPage) => prevPage + 1);
+      // Đẩy thanh scroll lên
+      window.scrollTo({
+        top: ref.current.offsetTop - 20, // Điều chỉnh giảm độ cao để thanh scroll không che phủ hoàn toàn nội dung mới
+        behavior: "smooth",
+      });
+    }, 1000); 
+  };
+
+    const infinityScroll = () =>{
+        const {scrollHeight , clientHeight , scrollTop } = ref.current
+        if((scrollHeight <= (clientHeight + scrollTop + 10)) && !isGreeting ){
+          fetchData();
+        }
+      }
+
+    useEffect(() =>{
+        ref.current.addEventListener('scroll' , infinityScroll)
+      },[isLoading])
+  
+    useEffect(()=> setDataScroll(data) , [data]);
+    return {dataScroll ,isGreeting};
+}
